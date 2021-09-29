3748779430c7ccf78343170470bdbdc2258dfe40
nathanlauder <laudern42@gmail.com>
2021-09-28 21:55:45 -0500

    add done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-#![allow(non_camel_case_types)]
-#![allow(dead_code)]
+#![allow(non_snake_case)]
 
-// 1:35:00 talking about enums
+// 2:03 talking about marking un/done
 
 use std::env;
 
@@ -35,19 +34,36 @@ impl TodoList {
   }
 
   fn print(&self) {
-    for item in &self.list {
-      println!("[{}] - {}", item.complete, item.name);
+    for (index, item) in self.list.iter().enumerate() {
+      println!("{} [{}] - {}", index, item.complete, item.name);
     }
   }
+
+  fn markDone (&mut self, index: usize) {
+    if self.list[index].complete == ' '{
+      self.list[index].complete = 'x';
+    }
+    else {
+      self.list[index].complete = ' ';
+    }
+  }
+
 }
 
-// enum Command {
-//   get: "get"
-// }
+enum Command {
+  Get,
+  Add(String),
+  Done(usize)
+}
 
 fn main() {
   let arguments: Vec<String> = env::args().collect();
-  let command = arguments[1].clone();
+  let command = match arguments[1].as_str() {
+    "get" => Command::Get,
+    "add" => Command::Add(arguments[2].clone()),
+    "done" => Command::Done(arguments[2].parse().expect("error converting to u32")),
+    _ => panic!("Provide proper command")
+  };
   let todoItem = "Hey Nathan".to_string();
   let todoItem2 = "Hey Nathan2".to_string();
   let mut todoList = TodoList::new();
@@ -55,19 +71,22 @@ fn main() {
   todoList.add(todoItem2);
   // println!("{:?}", todoItem);
   match command {
-    "get".to => todoList.print(),
-    "add" => {
-      let task = arguments[2].clone();
+    Command::Get => todoList.print(),
+    Command::Add(task) => {
       todoList.add(task);
+      todoList.print();
+    },
+    Command::Done(index) => {
+      todoList.markDone(index);
       todoList.print();
     }
   }
-  if command == "get" {
-    todoList.print();
-  }
-  else if command == "add" {
-    let task = arguments[2].clone();
-    todoList.add(task);
-    todoList.print();
-  }
+  // if command == "get" {
+  //   todoList.print();
+  // }
+  // else if command == "add" {
+  //   let task = arguments[2].clone();
+  //   todoList.add(task);
+  //   todoList.print();
+  // }
 }
